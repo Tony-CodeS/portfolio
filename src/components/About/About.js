e6dbf546eb7ea982157ef208a55bea79df67d5bb
Tony <asolomon484@gmail.com>
2023-01-31 13:12:15 +0100

    Portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useEffect } from "react";
+import Aos from 'aos';
+import "aos/dist/aos.css"
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import './About.css'
+
+
 function About() {
+
+  useEffect(() => {
+   Aos.init({duration:1000})
+  }, []);
+
   return (
-    <div id='About'>
-    <div id='aboutdiv'>
+
+ 
+    <div id='#About'>
+         <div>
+      <div id='aboutdiv' data-aos="fade-up">
         <h2 id='about-heading'> About Me</h2>
         <p id='about-writeup'> 
         My name is Anthony Aguanunu.
@@ -17,29 +30,31 @@ function About() {
         a team player and dedicate all my skills and talents to develop high-quality and unique products.
         </p>
     </div>
+  </div>
+
     <div id='skill-div'>
-        <h3 id='about-heading'> STACKS</h3>
+        <h3 id='about-heading' data-aos="flip-right"> STACKS</h3>
         <div id='stacks-div'>
         <h2 id='stacks'> HTML</h2>
         <ProgressBar animated now={95} id='progress' label={`95%`} variant='success'/>
         </div>
 
-        <div id='stacks-div'>
+        <div id='stacks-div' data-aos="fade-up">
         <h3 id='stacks'> CSS</h3>
         <ProgressBar animated now={75} id='progress' label={`75%`} variant='success'/>
         </div>
 
-        <div id='stacks-div'>
+        <div id='stacks-div' data-aos="fade-up">
         <h3 id='stacks'> BOOTSTRAP</h3>
         <ProgressBar animated now={85} id='progress' label={`85%`} variant='success'/>
         </div>
 
-        <div id='stacks-div'>
+        <div id='stacks-div' data-aos="fade-up">
         <h3 id='stacks'> JAVASCRIPT</h3>
         <ProgressBar animated now={55} id='progress' label={`55%`} variant='success'/>
         </div>
 
-        <div id='stacks-div'>
+        <div id='stacks-div' data-aos="fade-up">
         <h3 id='stacks'> REACT.JS</h3>
         <ProgressBar animated now={75} id='progress' label={`75%`} variant='success'/>
         </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useEffect } from 'react';
+import Aos from 'aos';
+import "aos/dist/aos.css"
 import {BsWhatsapp} from "react-icons/bs"
 import {BsTwitter} from "react-icons/bs"
 import {BsGithub} from "react-icons/bs"
@@ -10,6 +13,10 @@ import {TfiLinkedin} from 'react-icons/tfi'
 import './Home.css'
 
 function Home() {
+  
+  useEffect(() => {
+    Aos.init({duration:1000})
+   }, []);
   return (
   <div id='Homes'>
     <div id='mobileimage'>
@@ -18,29 +25,29 @@ function Home() {
         </div>
     </div>
       <div id='Home'>
-        <div id='intro-div'>
+        <div id='intro-div' data-aos="zoom-in">
             <h1> HELLO!! I'm Anthony...</h1>
             <p id='intro-p'> i am a full-stack web-developer,i work with react.js for nice user interface and node.js for API's</p>
             {/*button div  */}
             <div id='btn-div'>
-                <button id='hire-button'> <a href='https://wa.me/+2348088589379' id='hire-button-a'> Hire ME <CgProfile size={30} id='download-icon'/> </a> </button>
+                <button id='hire-button' data-aos="zoom-in"> <a href='https://wa.me/+2348088589379' id='hire-button-a'> Hire ME <CgProfile size={30} id='download-icon'/> </a> </button>
                 <button id='download-btn'>Download Resume <ImDownload size={30} id='download-icon'/> </button> 
             </div>
             {/*  */}
             <div>
                 <ul id='socials'>
-                    <li id='socials-li'> <a href='https://wa.me/+2348088589379' id='a'>  <BsWhatsapp size={30} id='aa'/></a>  </li>
-                    <li id='socials-li'> <a href='https://twitter.com/TonyAguanunu' id='a'> <BsTwitter size={30} id='aa'/></a>   </li>
-                    <li id='socials-li'> <a href='https://www.linkedin.com/in/anthony-solomon-b46681184/' id='a'> <TfiLinkedin size={30} id='aa'/></a>  </li>
-                    <li id='socials-li'> <a href='https://github.com/Tony-CodeS' id='a'> <BsGithub size={30} id='aa'/> </a> </li>
-                    <li id='socials-li'> <a href='mailto:asolomon484@gmail.com' id='a'> <SiGmail size={30} id='aa'/></a> </li>
+                    <li id='socials-li' data-aos="fade-in"> <a href='https://wa.me/+2348088589379' id='a'>  <BsWhatsapp size={30} id='aa'/></a>  </li>
+                    <li id='socials-li' data-aos="fade-in"> <a href='https://twitter.com/TonyAguanunu' id='a'> <BsTwitter size={30} id='aa'/></a>   </li>
+                    <li id='socials-li' data-aos="fade-in"> <a href='https://www.linkedin.com/in/anthony-solomon-b46681184/' id='a'> <TfiLinkedin size={30} id='aa'/></a>  </li>
+                    <li id='socials-li' data-aos="fade-in"> <a href='https://github.com/Tony-CodeS' id='a'> <BsGithub size={30} id='aa'/> </a> </li>
+                    <li id='socials-li' data-aos="fade-in"> <a href='mailto:asolomon484@gmail.com' id='a'> <SiGmail size={30} id='aa'/></a> </li>
                 </ul>
             </div>
         </div>
         {/* image */}
-        <div id='image-div'>
-        <div id='image-div2'>
-        <span id='image-span'></span>
+        <div id='image-div' data-aos="flip-right">
+        <div id='image-div2' data-aos="flip-right">
+        <span id='image-span' data-aos="flip-right"></span>
         </div>
         {/* <span id='image-span'> gggggggggggggggggggggggggggggggggggggggggg</span> */}
         </div>
